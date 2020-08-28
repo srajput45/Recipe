337ab0b1ed82c457b54957e32166bec690afabb2
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-08-29 01:05:07 +0530

    Updates

---
+++ precache-manifest.8b83cd68b53890a8e8bd0b2efb544180.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e497451eab649cba945c0e1d75658e83",
+    "url": "/Recipe/index.html"
+  },
+  {
+    "revision": "9c121f1e272f7f21336f",
+    "url": "/Recipe/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fb010c3aacac6e6007b2",
+    "url": "/Recipe/static/css/main.cc353547.chunk.css"
+  },
+  {
+    "revision": "9c121f1e272f7f21336f",
+    "url": "/Recipe/static/js/2.a5864ee0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Recipe/static/js/2.a5864ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb010c3aacac6e6007b2",
+    "url": "/Recipe/static/js/main.189a6e75.chunk.js"
+  },
+  {
+    "revision": "efc72d8c1e1b0dd60db5",
+    "url": "/Recipe/static/js/runtime-main.737d7316.js"
+  }
+]);
